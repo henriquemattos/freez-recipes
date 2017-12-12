7b1b3c0d26568ff20de077ad1967a79b7bebc9d1
Henrique Mattos <henrique.mattos@sap.com>
2017-12-12 23:52:36 +0100

    Added description on how to use shortcode parameters

---
+++ README.md
@@ -0,0 +1,14 @@
+# Freez Recipes
+Este plugin de receitas foi desenvolvido a pedidos da [Freez Creative Studio](http://freez.com.br/) para o portal [HomeChefs](http://homechefs.com.br/).
+
+## Como usar
+
+### Shortcodes
++ Para listar todas as receitas: `[freezrecipes]`.
++ Para listar uma receita específica: `[freezrecipes id=123]`.
++ Para listar várias receitas: `[freezrecipes id=123,124,125,126,265,275]`.
++ Para listar receitas com links: `[freezrecipes id=123 link="true"]`.
++ Para listar receitas com checkbox e botão de impressão: `[freezrecipes id=123,124,125 checkbox="true"]`.
++ Para listar receitas com checkbox, botão de impressão e links: `[freezrecipes id=123,124,125 checkbox="true" link="true"]`.
++ Para listar receitas com paginação `[freezrecipes pagination="true" perpage="10"]`.
+⋅⋅⋅ Paginação em desenvolvimento.
--- template-settings.php
+++ template-settings.php
@@ -1,4 +1,16 @@
 <h1>Freez Recipes - Configurações de Receitas</h1>
+<blockquote cite="https://github.com/visualworks/freez-recipes/">
+  <h2>Como utilizar os shortcodes</h2>
+  <ul>
+    <li>Para listar todas as receitas: <strong>[freezrecipes]</strong>.</li>
+    <li>Para listar uma receita específica: <strong>[freezrecipes id=123]</strong>.</li>
+    <li>Para listar várias receitas: <strong>[freezrecipes id=123,124,125,126,265,275]</strong>.</li>
+    <li>Para listar receitas com links: <strong>[freezrecipes id=123 link="true"]</strong>.</li>
+    <li>Para listar receitas com checkbox e botão de impressão: <strong>[freezrecipes id=123,124,125 checkbox="true"]</strong>.</li>
+    <li>Para listar receitas com checkbox, botão de impressão e links: <strong>[freezrecipes id=123,124,125 checkbox="true" link="true"]</strong>.</li>
+    <li>Para listar receitas com paginação <strong>[freezrecipes pagination="true" perpage="10"]</strong>. Paginação em desenvolvimento.</li>
+  </ul>
+</blockquote>
 <form action="<?php echo admin_url('options-general.php?page=freez_recipes_settings'); ?>" method="POST">
   <input type="hidden" id="freez-recipes-setting" name="action" value="freez_recipes_settings" />
   <div>
